24ba1b75cf04f953a83f6d180698f0e8f92c05af
Vy Tran <vytran@Vys-MacBook-Pro.local>
2024-10-09 10:12:36 -0500

    create v branch

---
+++ main.py
@@ -0,0 +1,6 @@
+def main(): 
+
+# Introduction 
+    print('Greetings, my aspiring magic users! The Tianyuan Academy of Magic has been a sanctuary for wizards, witches, and sorcerers for centuries, and we are thrilled to invite you to become a part of our enchanting community. You are about to be embark on a magical journey.')
+
+main()
