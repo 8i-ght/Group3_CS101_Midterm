108c3f81efece54c987134bdca067d8e1be9ac63
Kayden -_- <needboosts3545@gmail.com>
2024-10-11 10:43:51 -0500

    Added all questions

--- main.py
+++ main.py
@@ -22,7 +22,15 @@ def house_introduction():
 def quiz_questions():
     # Ask the user a series of questions inside the array to determine their house
     questions = [
-        "I feel I can take charge a lead a group succesfully (1. Strongly Agree 2. Agree 3. Disagree 4. Strongly Disagree): "
+        "I feel I can take charge a lead a group succesfully (1. Strongly Agree 2. Agree 3. Disagree 4. Strongly Disagree): ",
+        "When I'm dead I want people to remember me as: (1. The Good 2. The Great 3. The Wise 4. The Bold): ",
+        "Are you a night owl or an early bird? (1. Night Owl 2. Early Bird 3. It doesn't matter): ",
+        "Which of these traits do you value the most? (1. Power 2. Creativity 3. Loyalty 4. Wit): ",
+        "My friends would describe me as: (1. Ambitious 2. Creative 3. Loyal 4. Witty): ",
+        "If I got reincarnated, I would want to be: (1. A Dragon 2. A Phoenix 3. A Tiger 4. A Fox): ",
+        "How is success best achieved? (1. Through power 2. Through resilience 3. Through honor 4. Through Cleverness): ",
+        "I handle stress by (1. Taking charge 2. Being creative 3. Relying on others 4. Thinking my way out): ",
+        "I would rather be (1. Feared 2. Loved 3. Respected 4. Admired): "
     ]
 
     # Create a dictionary to store the scores of each house
