dc1980cc6bd92412f9af5bdabd2b541efac5e0ea
Kayden -_- <needboosts3545@gmail.com>
2024-10-13 12:09:34 -0500

    final changes

--- main.py
+++ main.py
@@ -5,19 +5,19 @@
 # You will also have the option to get a brief introduction of the different housing options
 # We will prompt the user to enter numbers to navigate through the menus and to answer the questions
 def starting_menu():
-        print('Greetings, my aspiring magic users! The Tianyuan Academy of Magic has been a sanctuary for wizards, witches, and sorcerers for centuries, and we are thrilled to invite you to become a part of our enchanting community. You are about to be embark on a magical journey.')
+        print('\nGreetings, my aspiring magic users! The Tianyuan Academy of Magic has been a sanctuary for wizards, witches, and sorcerers for centuries, and we are thrilled to invite you to become a part of our enchanting community. You are about to be embark on a magical journey.')
         print("1. Introduce the Houses")
         print("2. Take the Quiz")
         print("3. Exit")
-
+# Introductions to all the houses
 def house_introduction():
-
         print("\nHouse Introductions:")
         print("1. Dragon House: Power, Leadership, Ambition and Nobility.")
         print("2. Phoenix House: Hope, Resilience, Grace, Creativity.")
         print("3. Tiger House: Honor, Bravery, and Loyalty.")
         print("4. Fox House: Cunning, Wit, Charm\n")
 
+# Validator for menu navigation
 def validate_input(question_prompt, valid_options):
     while True:
         choice = input(question_prompt)
@@ -26,9 +26,9 @@ def validate_input(question_prompt, valid_options):
         else:
             print("Invalid input. Please try again.")
 
-
+# Function to start the quiz and ask the user a series of yes/no questions
 def start_quiz():
-    
+    # Function to validate the user's answer if not yes/no
     def validate_answer(question, valid_answer): 
         while True:
             answer = input(question)
@@ -36,20 +36,20 @@ def start_quiz():
                 return answer
             else: print("Invalid input please try again!")
 
-    bravery = validate_answer("Do you face your fear? (yes/no): ", ['yes', 'no']).strip()
-    cunning = validate_answer("Are you clever and skillful at achieving your goals by tricking or deceiving others? (yes/no): ", ['yes', 'no'])
-    loyalty = validate_answer("Are you faithful and steadfast in their allegiance to a person, cause, or ideal, even when faced with temptation to betray or desert? (yes/no): ", ['yes', 'no'])
-    ambition = validate_answer("Are you always striving to reach for a goal? (yes/no): ", ['yes', 'no'])
-    nobility = validate_answer("As a member of the society, do you belong in the highest social class? (yes/no): ", ['yes', 'no'])
-    honor = validate_answer("Do you have a strong sense of what is right and wrong, and try to live up to those principles? (yes/no): ", ['yes', 'no'])
-    charm = validate_answer("Do other people find you charismatic? (yes/no): ", ['yes', 'no'])
-    hope = validate_answer("In any circumstances, do you have a positive outlook and are persistent? (yes/no): ", ['yes', 'no'])
-    resilience = validate_answer("When faced with a difficult situation, are you able to adapt and recover from it? (yes/no): ", ['yes', 'no'])
-    creativity = validate_answer("Can you come up with original ideas and use them to create new things, often in an imaginative way? (yes/no): ", ['yes', 'no'])
-    wit = validate_answer("Are you quick and inventive in verbal humor? (yes/no): ", ['yes', 'no'])
+    bravery = validate_answer("\nDo you face your fear? (yes/no): ", ['yes', 'no']).strip()
+    cunning = validate_answer("\nAre you clever and skillful at achieving your goals by tricking or deceiving others? (yes/no): ", ['yes', 'no'])
+    loyalty = validate_answer("\nAre you faithful and steadfast in their allegiance to a person, cause, or ideal, even when faced with temptation to betray or desert? (yes/no): ", ['yes', 'no'])
+    ambition = validate_answer("\nAre you always striving to reach for a goal? (yes/no): ", ['yes', 'no'])
+    nobility = validate_answer("\nAs a member of the society, do you belong in the highest social class? (yes/no): ", ['yes', 'no'])
+    honor = validate_answer("\nDo you have a strong sense of what is right and wrong, and try to live up to those principles? (yes/no): ", ['yes', 'no'])
+    charm = validate_answer("\nDo other people find you charismatic? (yes/no): ", ['yes', 'no'])
+    hope = validate_answer("\nIn any circumstances, do you have a positive outlook and are persistent? (yes/no): ", ['yes', 'no'])
+    resilience = validate_answer("\nWhen faced with a difficult situation, are you able to adapt and recover from it? (yes/no): ", ['yes', 'no'])
+    creativity = validate_answer("\nCan you come up with original ideas and use them to create new things, often in an imaginative way? (yes/no): ", ['yes', 'no'])
+    wit = validate_answer("\nAre you quick and inventive in verbal humor? (yes/no): ", ['yes', 'no'])
   
 
-
+# Assign the user to a house based on there answers
     if bravery == "yes" and ambition == "yes" and nobility == "yes":
         house = "Dragon"
     elif hope == "yes" and resilience == "yes" and creativity == "yes" :
@@ -59,11 +59,11 @@ def start_quiz():
     elif cunning == "yes" and wit == "yes" and charm == "yes":
         house = "Fox"
     else:
-        house = "Unknown"
+        house = "You don't belong to any"
 
-    print(house)
+    print(f"\nCongratulations, you are now a part of the {house} house!")
 
-
+# Main function for running the program
 def main():
      while True:
         starting_menu()
