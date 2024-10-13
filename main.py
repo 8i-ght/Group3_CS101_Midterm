34823179598356c83915c57a2aa0f6cc126f19d7
Kayden -_- <needboosts3545@gmail.com>
2024-10-13 11:42:57 -0500

    change file name

--- main.py
+++ main.py
@@ -5,22 +5,19 @@
 # You will also have the option to get a brief introduction of the different housing options
 # We will prompt the user to enter numbers to navigate through the menus and to answer the questions
 def starting_menu():
-    # Display the starting menu to the users.
-    print("Welcome to The Tianyuan Academy of Magic housing assignment!")
-    print("1. Introduce the Houses")
-    print("2. Take the Quiz")
-    print("3. Exit")
+        print('Greetings, my aspiring magic users! The Tianyuan Academy of Magic has been a sanctuary for wizards, witches, and sorcerers for centuries, and we are thrilled to invite you to become a part of our enchanting community. You are about to be embark on a magical journey.')
+        print("1. Introduce the Houses")
+        print("2. Take the Quiz")
+        print("3. Exit")
 
 def house_introduction():
-    # Introduce each of the houses to the user
-    print("\nHouse Introductions:")
-    print("1. Dragon House: Power, leadership, ambition, nobility.")
-    print("2. Phoenix House: Hope, resilience, grace, creativity.")
-    print("3. Tiger House: Honor, bravery, loyalty.")
-    print("4. Fox House: Cunning, wit, charm.\n")
 
-# Create validation function to ensure that the user input is within the guidelines
-# Display the question to the user if the user input is valid add the score if not valid display an error message
+        print("\nHouse Introductions:")
+        print("1. Dragon House: Power, Leadership, Ambition and Nobility.")
+        print("2. Phoenix House: Hope, Resilience, Grace, Creativity.")
+        print("3. Tiger House: Honor, Bravery, and Loyalty.")
+        print("4. Fox House: Cunning, Wit, Charm\n")
+
 def validate_input(question_prompt, valid_options):
     while True:
         choice = input(question_prompt)
@@ -30,66 +27,52 @@ def validate_input(question_prompt, valid_options):
             print("Invalid input. Please try again.")
 
 
-def quiz_questions():
-    # Ask the user a series of questions inside the array to determine their house
-    questions = [
-        "I feel I can take charge a lead a group succesfully (1. Strongly Agree 2. Agree 3. Disagree 4. Strongly Disagree): ",
-        "When I'm dead I want people to remember me as: (1. The Good 2. The Great 3. The Wise 4. The Bold): ",
-        "Are you a night owl or an early bird? (1. Night Owl 2. Early Bird 3. It doesn't matter): ",
-        "Which of these traits do you value the most? (1. Power 2. Creativity 3. Loyalty 4. Wit): ",
-        "My friends would describe me as: (1. Ambitious 2. Creative 3. Loyal 4. Witty): ",
-        "If I got reincarnated, I would want to be: (1. A Dragon 2. A Phoenix 3. A Tiger 4. A Fox): ",
-        "How is success best achieved? (1. Through power 2. Through resilience 3. Through honor 4. Through Cleverness): ",
-        "I handle stress by (1. Taking charge 2. Being creative 3. Relying on others 4. Thinking my way out): ",
-        "I would rather be (1. Feared 2. Loved 3. Respected 4. Admired): "
-    ]
-
-    # Create a dictionary to store the scores of each house
-    scores = {"Dragon House": 0, "Phoenix House": 0, "Tiger House": 0, "Fox House": 0}
-
-
-    # Loop through the questions and add the score to the correct house
-    # Using the validate_input function to ensure the user input is valid
-    # Question arugment shows the user the question prompt the array of numbers are the valid options
-    for question in questions:
-        answer = validate_input(question, ['1', '2', '3', '4'])
-        if answer == '1':
-            scores["Dragon House"] += 1
-        elif answer == '2':
-            scores["Phoenix House"] += 1
-        elif answer == '3':
-            scores["Tiger House"] += 1
-        elif answer == '4':
-            scores["Fox House"] += 1
-
-    assign_user(scores)
+def start_quiz():
     
+    def validate_answer(question, valid_answer): 
+        while True:
+            answer = input(question)
+            if answer in valid_answer:
+                return answer
+            else: print("Invalid input please try again!")
 
-# Assign the user to the house with the highest score
-def assign_user(scores):
-    highest_score = -1
-    assigned_house = None
-# For loop to go through the the key values in the scores dictionary
-    for house, score in scores.items():
-        if score > highest_score:
-            highest_score = score
-            assigned_house = house
+    bravery = validate_answer("Do you face your fear? (yes/no): ", ['yes', 'no']).strip()
+    cunning = validate_answer("Are you clever and skillful at achieving your goals by tricking or deceiving others? (yes/no): ", ['yes', 'no'])
+    loyalty = validate_answer("Are you faithful and steadfast in their allegiance to a person, cause, or ideal, even when faced with temptation to betray or desert? (yes/no): ", ['yes', 'no'])
+    ambition = validate_answer("Are you always striving to reach for a goal? (yes/no): ", ['yes', 'no'])
+    nobility = validate_answer("As a member of the society, do you belong in the highest social class? (yes/no): ", ['yes', 'no'])
+    honor = validate_answer("Do you have a strong sense of what is right and wrong, and try to live up to those principles? (yes/no): ", ['yes', 'no'])
+    charm = validate_answer("Do other people find you charismatic? (yes/no): ", ['yes', 'no'])
+    hope = validate_answer("In any circumstances, do you have a positive outlook and are persistent? (yes/no): ", ['yes', 'no'])
+    resilience = validate_answer("When faced with a difficult situation, are you able to adapt and recover from it? (yes/no): ", ['yes', 'no'])
+    creativity = validate_answer("Can you come up with original ideas and use them to create new things, often in an imaginative way? (yes/no): ", ['yes', 'no'])
+    wit = validate_answer("Are you quick and inventive in verbal humor? (yes/no): ", ['yes', 'no'])
+  
 
-    print(f"Congratulations! You have been assigned to {assigned_house}!")
+
+    if bravery == "yes" and ambition == "yes" and nobility == "yes":
+        house = "Dragon"
+    elif hope == "yes" and resilience == "yes" and creativity == "yes" :
+        house = "Phoenix"
+    elif honor == "yes" and bravery == "yes" and loyalty == "yes":
+        house = "Tiger"
+    elif cunning == "yes" and wit == "yes" and charm == "yes":
+        house = "Fox"
+    else:
+        house = "Unknown"
+
+    print(house)
 
 
-
-# Main function to run the program 
 def main():
-    while True:
+     while True:
         starting_menu()
         choice = validate_input("Please choose an option (1-3): ", ['1', '2', '3'])
         if choice == '1':
             house_introduction()
         elif choice == '2':
-            quiz_questions()
+            start_quiz()
         elif choice == '3':
             print("Thank you for taking the quiz. Goodbye!")
             break
-
 main()
